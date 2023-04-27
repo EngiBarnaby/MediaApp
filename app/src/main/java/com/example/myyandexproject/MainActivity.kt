d498771cab631088a720d21a6f1d915b7e769f61
EngiBarnaby <artem@itpw.ru>
2023-04-27 08:27:14 +0300

    First commit

--- app/src/main/java/com/example/myyandexproject/MainActivity.kt
+++ app/src/main/java/com/example/myyandexproject/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.myyandexproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchBtn = findViewById<Button>(R.id.searchBtn)
+
+        searchBtn.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
     }
 }
